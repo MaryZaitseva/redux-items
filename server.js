77cc62e9078980af0f1ccaded61ff3cdb047c5da
unknown <m.zaitseva@do-it.co>
2018-06-14 11:13:37 +0300

    connected server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const socketServer =require('socket.io')
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+var serve = http.createServer(app);
+serve.listen(5000,()=> {console.log("Express Server Running!")})
+
+mongoose.connect('mongodb://localhost:27017/local')
+var db = mongoose.connection
+db.on('error', ()=> {console.log( 'FAILED to connect to mongoose')})
+db.once('open', () => {
+	console.log( 'Connected to mongoose')
+})
